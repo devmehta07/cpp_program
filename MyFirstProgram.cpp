daa3bd8d7c0afa6ab28e47f557e44ef96c9fc332
iD Student <devvmehta711@gmail.com>
2017-07-03 16:21:08 -0400

    hey

--- MyFirstProgram.cpp
+++ MyFirstProgram.cpp
@@ -7,8 +7,13 @@ int main()
 	string advice = "Run";
 	string advice2 = "Find a safe house preferably on a deserted island, lock the doors and windows and barricade them. Don't worry about me, I'll be fine. Now go, and don't stop for anything. ";
     string backup = "Grab a very sharp knife and jab it at the bread.";
+    
+    cout << "Enter some advice: " << endl;
+    cin >> advice;
+    
     cout << "You want my advice?" << endl;
     cout << "My Advice: " << advice2 << endl;
     cout << backup << endl;
+    cout << advice << endl;
    
 }
