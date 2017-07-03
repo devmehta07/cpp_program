816e74d9be9c0048e30f740bd6d6469db1d6aa15
iD Student <devvmehta711@gmail.com>
2017-07-03 16:10:45 -0400

    ok

--- MyFirstProgram.cpp
+++ MyFirstProgram.cpp
@@ -1,9 +1,14 @@
 #include <iostream> 
-
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Welcome to my first program!" << endl;
-    cout << "This has been a success!" << endl;
+	string advice = "Run";
+	string advice2 = "Find a safe house preferably on a deserted island, lock the doors and windows and barricade them. Don't worry about me, I'll be fine. Now go, and don't stop for anything. ";
+    string backup = "Grab a very sharp knife and jab it at the bread.";
+    cout << "You want my advice?" << endl;
+    cout << "My Advice: " << advice2 << endl;
+    cout << backup << endl;
+   
 }
